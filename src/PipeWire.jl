70d59e6bd6a78550b2fb364cd817c967d75fa958
L. Nikko <lpn@users.noreply.github.com>
2024-09-10 02:24:19 +0300

    initial commit

--- src/PipeWire.jl
+++ src/PipeWire.jl
@@ -1,5 +1,68 @@
 module PipeWire
 
-# Write your package code here.
+using Timers
+
+jl2pw(T) = error("Unsupported stream format: $T")
+# jl2pw(::Type{Int8}) = :S8 # broken
+jl2pw(::Type{Int16}) = :S16LE
+jl2pw(::Type{Int32}) = :S32LE
+jl2pw(::Type{Float32}) = :F32LE
+jl2pw(::Type{Float64}) = :F64LE
+
+struct PipeTunnel
+    process::Base.Process
+    stream::IOStream
+    filename::String
+    props::NamedTuple
+end
+
+function PipeTunnel(; name::String="julia-pw", mode=:playback, latency::T=1024, format=Float64, channels::T=2, rate::T=48000, position::String="[ FL FR ]", maypause::Bool=true, filename::String=string(tempname(), "_pipewire")) where {T<:Integer}
+    ispath(filename) && error("pipe \"$filename\" already exists!")
+
+    cmd = Cmd(`pw-cli -m \
+        load-module libpipewire-module-pipe-tunnel \
+        node.name=\"$name\" \
+        pipe.filename=\"$filename\" \
+        tunnel.mode=$mode \
+        tunnel.may-pause=$maypause \
+        node.latency=$latency \
+        audio.channels=$channels \
+        audio.rate=$rate \
+        audio.position=$position \
+        audio.format=$(jl2pw(format))`)
+
+    process = run(cmd; wait=false)
+    run(`mkfifo $filename`)
+    handle = open(filename, "w")
+    props = (
+        name=name,
+        mode=mode,
+        latency=latency,
+        format=format,
+        channels=channels,
+        rate=rate,
+        position=position
+    )
+
+    PipeTunnel(process, handle, filename, props)
+end
+
+function Base.close(p::PipeTunnel)
+    close(p.stream)
+    kill(p.process)
+    rm(p.filename)
+end
+
+Base.write(pw::PipeTunnel, xs) = write(pw.stream, xs |> htol)
+
+function Base.Channel(pw::PipeTunnel, n=2)
+    latency_ns = 1e9 * pw.props.latency / pw.props.rate
+    Channel{Vector{pw.props.format}}(n) do buffers
+        foreach(buffers) do buffer
+            write(pw, buffer)
+            wait_until(time_ns() + latency_ns)
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,33 @@
 using PipeWire
 using Test
 
+function tone(format, length=3; conv=identity)
+    pw = PipeWire.PipeTunnel(; latency=1024, rate=48000, format=format)
+    chnl = Channel(pw)
+
+    @show pw
+
+    generate(t, f=440) = sin(f * 2pi * t / pw.props.rate) * exp2(-2.25) |> conv
+
+    latency = pw.props.latency
+    iterations = div(length * pw.props.rate, latency)
+
+    @time foreach(1:iterations) do i
+        xs = [generate(latency * i + x, 220 * exp2(ch * exp2(-6.5))) for x in 1:latency for ch in -1:2:1]
+        put!(chnl, xs)
+    end
+
+    close(pw)
+
+    true
+end
+
+tone(format::AbstractFloat) = tone(format; conv=identity)
+tone(format::Type{T}) where {T<:Signed} = tone(format; conv=conv = x -> round(format, typemax(format) * x))
+
 @testset "PipeWire.jl" begin
-    # Write your tests here.
+    @test tone(Float64)
+    @test tone(Float32)
+    @test tone(Int32)
+    @test tone(Int16)
 end
